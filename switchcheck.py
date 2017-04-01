b8f59a3d4be170f5fb2862a875652379d0261fe1
James Chant <jamesrchant@gmail.com>
2017-04-01 22:10:52 +0100

    Version working with Amazon, minus Twilio details

--- switchcheck.py
+++ switchcheck.py
@@ -12,8 +12,8 @@ print("Begin check")
 
 
 def check():
-    account_sid = "AC542e629143b6f86ab8b0c92b01fbfbfd"
-    auth_token = "81b095f826a95ace93ab28257e5b9aa6"
+    account_sid = ""
+    auth_token = ""
     client = TwilioRestClient(account_sid, auth_token)
     page = requests.get('https://www.amazon.co.uk/Nintendo-UK-NSHEHWNIN45229-Switch-Grey/dp/B01MFADJFV/')
     html_contents = str(page.text)
@@ -25,8 +25,8 @@ def check():
         stock = True
         print("Item in stock.")
         client.messages.create(
-            to="+447796896439",
-            from_="+441304410174",
+            to="",
+            from_="",
             body="The Nintendo Switch is in stock on Amazon UK!")
 
 # Set the time interval for check to run
