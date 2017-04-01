fe40c5c64850b8536a6dd3056d4208861a38c783
James Chant <jamesrchant@gmail.com>
2017-04-01 11:20:35 +0100

    Working file. Program checks web page for text, then if present sends a text message.

--- switchcheck.py
+++ switchcheck.py
@@ -2,33 +2,36 @@ import schedule
 import time
 from urllib.request import urlopen
 import re
-# from twilio.rest import TwilioRestClient
+from twilio.rest import TwilioRestClient
 
 stock = False
 
 
 def check():
-    with urlopen('https://www.reddit.com/r/FIREUK/comments/5hqszm/pension_contributions/') as f:
+    account_sid = "XX"
+    auth_token = "YY"
+    client = TwilioRestClient(account_sid, auth_token)
+    with urlopen('https://www.amazon.co.uk/Nintendo-UK-NSHEHWNIN45229-Switch-Grey/dp/B01MFADJFV/') as f:
         site = f.read()
-        textonpage = re.findall(b'boots', site)
-    # client = TwilioRestClient("XX", "YY")
+        textonpage = re.findall(b'In Stock', site)
     if len(textonpage) == 0:
-        print("Not there.")
+        print("Item not in stock.")
     else:
         global stock
         stock = True
-        print("It's in stock.")
-        # If that string is there send a text to my number
-        # client.messages.create(to="XX", from_="YY", body="The Nintendo Switch is in stock on Amazon UK!")
+        print("Item in stock.")
+        client.messages.create(
+            to="",
+            from_="",
+            body="The Nintendo Switch is in stock on Amazon UK!")
+
+# Set the time interval for check to run
+schedule.every(5).minutes.do(check)
 
 
-# Set the time interval to run on 
-schedule.every(20).seconds.do(check)
-
-
-# If there is no stock, run the check until there is.
-while stock == False:
-    # Set the time interval to run on
-    print("Running")
+# Starting with the assumption there is no stock, run the check until there is
+while stock is False:
+    # Schedule a call of the check function
+    # print("Running")
     schedule.run_pending()
     time.sleep(1)
